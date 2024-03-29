5f7c313cdd13b1ea52ab222c1fdc2b6a0aacc926
prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-03-29 19:27:43 +0530

    implement Deref for TokenWrapper

--- backend/src/utilities/token_wrapper.rs
+++ backend/src/utilities/token_wrapper.rs
@@ -1,2 +1,12 @@
+use std::ops::Deref;
+
 #[derive(Clone)]
 pub struct TokenWrapper(pub String);
+
+impl Deref for TokenWrapper {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
